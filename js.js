b700b4379bb074219de94b2918e3ca8ff1c50542
Jovlonbek <javlonxolmurodov2000@gamil.com>
2022-04-05 20:34:20 +0500

    gj

---
+++ js.js
@@ -0,0 +1,14 @@
+let wrapper = document.querySelector(".wrapper")
+fetch("https://fake-movie-database-api.herokuapp.com/api?s=batman").
+then(res => res.json())
+.then(users =>
+    users.Search.forEach(item => {
+        wrapper.innerHTML +=`
+        <div class="card">
+        <div class="img"><img src="${item.Poster}" alt=""></div>
+           <h2>${item.Title}</h2>
+           <p>${item.Year}</p>
+        </div>`
+    })
+    
+) 
